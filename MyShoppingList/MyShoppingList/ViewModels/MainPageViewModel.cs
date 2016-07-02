d26691ec87cefa90555ccb4f285ef2786ed0e708
h0Sh <default_hoshi@gmx.de>
2016-07-03 00:40:03 +0200

    Create solution

---
+++ MyShoppingList/MyShoppingList/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using PropertyChanged;
+
+namespace MyShoppingList.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
